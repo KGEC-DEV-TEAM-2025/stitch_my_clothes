3c88b749b4337bea3d21adbad9848255335216b8
Arka Dash <dasharka05@gmail.com>
2025-02-05 16:03:10 +0530

    feat: add ESLint configuration to ignore checks during builds

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["example.com", "res.cloudinary.com"]
 
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint checks during builds (e.g., on Vercel)
+  }, 
   /* config options here */
 };
 
